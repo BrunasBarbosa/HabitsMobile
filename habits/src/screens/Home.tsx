c07abdef9cb3d954ea5337913b731e967b6ff425
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-01-22 18:39:26 -0300

    create progress bar

--- habits/src/screens/Home.tsx
+++ habits/src/screens/Home.tsx
@@ -1,7 +1,8 @@
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 import { HabitDay, DAY_SIZE } from '../components/HabitDay';
-import { Header } from '../components/Header';
 import { View, Text, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Header } from '../components/Header';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearStart = generateRangeDatesFromYearStart();
@@ -9,6 +10,8 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -27,15 +30,17 @@ export function Home() {
         }
       </View>
 
-      <ScrollView 
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingBottom: 100}}
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       >
+
         <View className="flex-row flex-wrap">
           {
             datesFromYearStart.map(date => (
               <HabitDay
                 key={date.toISOString()}
+                onPress={() => navigate('habit', { date: date.toISOString() })}
               />
             ))
           }
--- habits/src/screens/New.tsx
+++ habits/src/screens/New.tsx
@@ -58,6 +58,7 @@ export function New() {
 
         <TouchableOpacity
           className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+          activeOpacity={0.7}
         >
           <Feather
             name="check"
