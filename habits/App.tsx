b0b4a9e07b1e9148f0c9336d91df78420d7d4c6a
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-01-21 22:57:50 -0300

    create Header component

--- habits/App.tsx
+++ habits/App.tsx
@@ -1,5 +1,6 @@
 import { Loading } from './src/components/Loading';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Home } from './src/screens/Home';
+import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 
 export default function App() {
@@ -17,27 +18,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{`
-        DON'T GIVE UP!
-        KEEP GOING!
-      `}
-      </Text>
+    <>
+      <Home />
       <StatusBar barStyle='light-content' backgroundColor='transparent' translucent/>
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: '#fff',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
