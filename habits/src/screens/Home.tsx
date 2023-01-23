3668e28dd1fc4e7d17a3c46643f0981bb9cf54ba
Bruna Barbosa <brunas_barbosa@hotmail.com>
2023-01-22 21:37:51 -0300

    create api

--- habits/src/screens/Home.tsx
+++ habits/src/screens/Home.tsx
@@ -1,8 +1,10 @@
 import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates';
 import { HabitDay, DAY_SIZE } from '../components/HabitDay';
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Header } from '../components/Header';
+import { useState, useEffect } from 'react';
+import { api } from '../lib/axios';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
 const datesFromYearStart = generateRangeDatesFromYearStart();
@@ -10,7 +12,27 @@ const minimumSummaryDatesSizes = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearStart.length;
 
 export function Home() {
+  const [loading, setLoading] = useState(true);
+  const [summary, setSummary] = useState(null);
+
   const { navigate } = useNavigation();
+
+  async function fetchData() {
+    try {
+      setLoading(true);
+      const response = await api.get('/summary');
+      setSummary(response.data);
+    } catch (error) {
+      Alert.alert('Ops', 'Não foi possível carregar o sumário de hábitos.');
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return (
     <View className="flex-1 bg-background px-8 pt-16">
